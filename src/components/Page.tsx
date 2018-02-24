c191090890eb59b2d68eb34a89f9e4e27753cc05
泽荣 <foru.fy@gmail.com>
2018-02-24 17:40:44 +0800

    Abstract Jump Man

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,3 @@
-import PropTypes = require('prop-types');
-import qs = require('querystring');
-
 import * as marked from 'marked';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
@@ -9,25 +6,20 @@ import config from '../config';
 import { fetchFile, isTest, jumpTo } from '../lib/helper';
 
 import PageLayout from '../components/PageLayout';
+import JumpMan from '../container/JumpMan';
 import Toc from '../container/Toc';
 
 export type PageProps = RouteComponentProps<any>;
 export interface PageState {
   docContent: string;
   docHeadings: any[];
-  activeId: string;
 }
 export default class Page extends React.Component<PageProps, PageState> {
-  public static childContextTypes = {
-    activeId: PropTypes.string,
-    updateActiveId: PropTypes.func
-  };
   private toc: Hell.Heading[] = [];
 
   constructor(props: PageProps) {
     super(props);
     this.state = {
-      activeId: '',
       docContent: '',
       docHeadings: []
     };
@@ -40,27 +32,15 @@ export default class Page extends React.Component<PageProps, PageState> {
     const { location: { pathname } } = this.props;
     return this.formatPath(pathname);
   }
-  public getChildContext() {
-    return {
-      activeId: this.state.activeId,
-      updateActiveId: this.updateActiveId.bind(this)
-    };
-  }
   public componentDidUpdate({ location: { pathname } }) {
     if (this.formatPath(pathname + '') !== this.path) {
       this.fetchData();
     }
   }
-  public updateActiveId(activeId: string) {
-    jumpTo(activeId);
-    this.setState({ activeId });
-  }
   public componentDidMount() {
     const { location: { search } } = this.props;
-    const id = qs.parse(search.slice(1)).id as string;
-
     this.fetchData().then(() => {
-      this.updateActiveId(id);
+      // this.updateActiveId(id);
     });
   }
   public async fetchData() {
@@ -79,14 +59,23 @@ export default class Page extends React.Component<PageProps, PageState> {
     const { location } = this.props;
     return (
       <PageLayout
-        Side={<Toc headings={this.state.docHeadings} />}
-        Content={<div className="markdown-body section" dangerouslySetInnerHTML={{ __html: this.state.docContent }} />}
+        Side={
+          <JumpMan search={location.search}>
+            <Toc headings={this.state.docHeadings} />
+          </JumpMan>
+        }
+        Content={
+          <div
+            className="markdown-body section"
+            dangerouslySetInnerHTML={{ __html: this.state.docContent }}
+          />
+        }
       />
     );
   }
 
   private headingRender(text, level) {
-    const id = `heading-${text}-${level}`;
+    const id = text;
     const heading: Hell.Heading = { text, level, id };
 
     for (let idx = this.toc.length; idx > 0; idx--) {
