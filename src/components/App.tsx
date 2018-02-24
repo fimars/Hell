eb064de68442b3cc9497448252e98192f0a0898d
泽荣 <foru.fy@gmail.com>
2018-02-24 17:40:57 +0800

    tweaks App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,20 @@
 /// <reference path="../types.ts"/>
 
-import * as React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import * as React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
-import NotFound from "./NotFound";
-import Page from "./Page";
+import NotFound from './NotFound';
+import Page from './Page';
 
-export default () => (
-    <Router>
+export default class App extends React.PureComponent {
+  render() {
+    return (
+      <Router>
         <Switch>
-            <Route exact path="/404" component={NotFound} />
-            <Route component={Page} />
+          <Route exact path="/404" component={NotFound} />
+          <Route component={Page} />
         </Switch>
-    </Router>
-);
+      </Router>
+    );
+  }
+}
