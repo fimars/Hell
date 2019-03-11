684ee804cc78ec5d87056c0f636a19dfe0c7e7fe
fimars <foru.fy@gmail.com>
2019-03-11 17:41:00 +0800

    feat($cli): add default sourceDir

--- packages/helldoc-cli/src/index.ts
+++ packages/helldoc-cli/src/index.ts
@@ -3,17 +3,17 @@
 import * as program from "commander";
 import { prod, dev } from "@helldoc/core";
 
-program.version("0.0.1");
+program.version("0.2.11");
 
-bindUniversalOptions(program.command("dev <dir>"))
+bindUniversalOptions(program.command("dev [dir]"))
   .description("run the docs dev server with dir path")
-  .action(function(dir, options) {
+  .action(function(dir = process.cwd(), options) {
     dev(dir, options);
   });
 
-bindUniversalOptions(program.command("prod <dir>"))
+bindUniversalOptions(program.command("prod [dir]"))
   .description("output the static web files with dir path")
-  .action(function(dir, options) {
+  .action(function(dir = process.cwd(), options) {
     prod(dir, options);
   });
 
