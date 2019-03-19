d7c7241ddfa536d6b3ab12ff19e08f16ed13de58
fimars <foru.fy@gmail.com>
2019-03-19 22:39:48 +0800

    refactor(remove repeat function):

--- packages/helldoc-core/src/app/components/NavBar.tsx
+++ packages/helldoc-core/src/app/components/NavBar.tsx
@@ -1,23 +1,13 @@
-import siteData from "@internal/site-data";
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import actions from "../reduxActions/actions";
+import { resolveNavs } from "../util";
 
 class NavBar extends React.Component<{
   sidebarControl: (value: boolean) => void;
   sideBarDisplay: boolean;
 }> {
-  resolveNavs() {
-    if (siteData && siteData.themeConfig) {
-      return siteData.themeConfig.nav || [];
-    } else {
-      return siteData.pages
-        .filter(page => page.title)
-        .map(page => ({ text: page.title, link: page.path }));
-    }
-  }
-
   barMeunClick() {
     this.props.sidebarControl(true);
   }
@@ -31,7 +21,7 @@ class NavBar extends React.Component<{
   }
 
   render() {
-    const navs = this.resolveNavs();
+    const navs = resolveNavs();
     return (
       <div className="navbarstyle">
         <i
