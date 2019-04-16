7306ec79b53f2a7831c67707e15b2b6632708fea
fimars <foru.fy@gmail.com>
2019-04-16 18:28:31 +0800

    set default nav

--- packages/theme-default/src/themeConfig.ts
+++ packages/theme-default/src/themeConfig.ts
@@ -1,7 +1,9 @@
 // @ts-ignore
 import { siteData } from "@internal/runtime";
 
+const themeConfig = siteData.themeConfig || {};
+
 export const nav: Array<{
   text: string;
   link: string;
-}> = siteData.themeConfig.nav;
+}> = themeConfig.nav || [];
